4b5b31ddffad99a1e811a0880729c56699697d95
naveen7607 <naveen03082005@gmail.com>
2026-05-01 15:38:29 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,15 @@
-import UserProfile from "./components/userprofiles.jsx";
-// import "./App.css";
+import {BrowserRouter, Route, Routes} from 'react-router'
 
-const userDetailsList = [
-  {
-    uniqueNo: 1,
-    imageUrl: "https://assets.ccbp.in/frontend/react-js/esther-howard-img.png",
-    name: "Esther Howard",
-    role: "Software Developer",
-  },
-  {
-    uniqueNo: 2,
-    imageUrl: "https://assets.ccbp.in/frontend/react-js/floyd-miles-img.png",
-    name: "Floyd Miles",
-    role: "Software Developer",
-  },
-  {
-    uniqueNo: 3,
-    imageUrl: "https://assets.ccbp.in/frontend/react-js/jacob-jones-img.png",
-    name: "Jacob Jones",
-    role: "Software Developer",
-  },
-  {
-    uniqueNo: 4,
-    imageUrl: "https://assets.ccbp.in/frontend/react-js/devon-lane-img.png",
-    name: "Devon Lane",
-    role: "Software Developer",
-  },
-];
+import Home from './components/Home'
+
+import './App.css'
 
 const App = () => (
-  <div className="list-container">
-    <h1 className="title">Users List</h1>
-    {userDetailsList.map((eachItem) => (
-      <UserProfile userDetails={eachItem} key={eachItem.uniqueNo} />
-    ))}
-  </div>
-);
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
+  </BrowserRouter>
+)
 
-export default App;
+export default App
